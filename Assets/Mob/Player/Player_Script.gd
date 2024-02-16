9adebb15312e6ff0e282f1304964189b16aadff7
jus2tomate83 <blefebvre595@gmail.com>
2024-02-16 19:51:35 +0100

    Player/Debut Pnj

---
+++ Assets/Mob/Player/Player_Script.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+@export var SPEED = 200
+
+var screen_size = get_viewport_rect().size
+var direction = Vector2.ZERO
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const JUMP_VELOCITY = -200
+
+
+
+func _physics_process(delta):
+	var direction = Input.get_axis("gauche", "droite")
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+	if velocity.length() > 0 and is_on_floor() :
+		$AnimatedSprite2D.play("run")
+	else :
+		$AnimatedSprite2D.play("idle")
+	if velocity.x !=0 :
+		$AnimatedSprite2D.flip_h= velocity.x < 0
---
+++ Assets/Mob/Pnj/Pnj_Script.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+@export var speed = 200
+var direction = 1
+
+onready var path_follow = $Path2D/PathFollow2D
+
+func _physics_process(delta):
+	path_follow.offset += speed * delta * direction
+
+	if path_follow.unit_offset >= 1 and direction == 1:
+		direction = -1
+	elif path_follow.unit_offset <= 0 and direction == -1:
+		direction = 1
+
+	position = path_follow.position
