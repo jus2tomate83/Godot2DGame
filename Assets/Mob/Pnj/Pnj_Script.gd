733acab9b4d35770452109fb12150a58e2b655e8
jus2tomate83 <blefebvre595@gmail.com>
2024-02-16 22:45:30 +0100

    PNJ
    Pathfinding Pnj

--- Assets/Mob/Pnj/Pnj_Script.gd
+++ Assets/Mob/Pnj/Pnj_Script.gd
@@ -1,15 +1,17 @@
 extends CharacterBody2D
-@export var speed = 200
+@export var speed = 40
 var direction = 1
 
 @onready var path_follow = $Path2D/PathFollow2D
 
 func _physics_process(delta):
-	path_follow.offset += speed * delta * direction
+	var progress = path_follow.get_progress()
+	progress += speed * delta * direction
 
-	if path_follow.unit_offset >= 1 and direction == 1:
+	if progress >= 1 and direction == 1:
 		direction = -1
-	elif path_follow.unit_offset <= 0 and direction == -1:
+	elif progress <= 0 and direction == -1:
 		direction = 1
 
+	path_follow.set_progress(progress)
 	position = path_follow.position
