c63795ba0054645774b2f1f1c5f373d656cb5abb
jus2tomate83 <121179070+jus2tomate83@users.noreply.github.com>
2024-02-19 11:19:07 +0100

    update Le pnj se retourne maintenant

--- Mob/Pnj/PnjPathFollow/CharacterFollower2D.gd
+++ Mob/Pnj/PnjPathFollow/CharacterFollower2D.gd
@@ -1,10 +1,19 @@
 extends PathFollow2D
 var velocity = Vector2.ZERO
 @export var runSpeed = 300
+var last_position
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	last_position = position[0]
 	set_progress(get_progress() + runSpeed*delta)
+	
+	if position[0] < last_position:
+			$Pnj/AnimatedSprite2D.flip_h = true
+
+	if position[0] > last_position:
+		$Pnj/AnimatedSprite2D.flip_h = false
+
--- Mob/Pnj/PnjSprite/PNJ.gd
+++ Mob/Pnj/PnjSprite/PNJ.gd
@@ -1,8 +1,4 @@
 extends CharacterBody2D
-
+var last_position
 func _process(delta):
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = true
-		
-	if velocity.x > 0:
-		$AnimatedSprite2D.flip_h = false
+	pass
