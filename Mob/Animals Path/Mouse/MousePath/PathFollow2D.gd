ded27d4245df2b9f77b04365dfd2f3f46c0b0e12
jus2tomate83 <blefebvre595@gmail.com>
2024-02-21 16:52:11 +0100

    Mouse Idle Option

--- Mob/Animals Path/Mouse/MousePath/PathFollow2D.gd
+++ Mob/Animals Path/Mouse/MousePath/PathFollow2D.gd
@@ -1,8 +1,11 @@
 extends PathFollow2D
 var velocity = Vector2.ZERO
 @export var runSpeed = 300
+@export var idle_mouse = false
 var last_position
 var Mouse_pos_start
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Mouse_pos_start =$MouseSprite/AnimatedSprite2D.position
@@ -10,14 +13,18 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	last_position = position[0]
-	set_progress(get_progress() + runSpeed*delta)
-	if position[0] < last_position:
-		$MouseSprite/AnimatedSprite2D.flip_v = true
+	if idle_mouse == true:
+		$MouseSprite/AnimatedSprite2D.play("Idle")
+	if idle_mouse == false:
+		$MouseSprite/AnimatedSprite2D.play("Run")
+		last_position = position[0]
+		set_progress(get_progress() + runSpeed*delta)
+		if position[0] < last_position:
+			$MouseSprite/AnimatedSprite2D.flip_v = true
 		
+		if position[0] > last_position:
+			$MouseSprite/AnimatedSprite2D.flip_v = false
+		if progress_ratio == 1:
+			while progress_ratio != 0:
+					set_progress(get_progress() - runSpeed*delta)
 
-	if position[0] > last_position:
-		$MouseSprite/AnimatedSprite2D.flip_v = false
-	if progress_ratio == 1:
-		while progress_ratio != 0:
-				set_progress(get_progress() - runSpeed*delta)
--- Mob/Animals Path/Mouse/MouseSprite/MouseSprite.gd
+++ Mob/Animals Path/Mouse/MouseSprite/MouseSprite.gd
@@ -1,4 +1,4 @@
 extends Sprite2D
 
 func _physics_process(delta):
-	$AnimatedSprite2D.play("Run")
+	pass
