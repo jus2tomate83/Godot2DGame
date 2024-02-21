946a068723261b0871a2ac50e14c4150b927138c
jus2tomate83 <blefebvre595@gmail.com>
2024-02-21 14:49:31 +0100

    Tel Mouvement Finish

--- Mob/Pnj/PnjPathFollow/CharacterFollower2D.gd
+++ Mob/Pnj/PnjPathFollow/CharacterFollower2D.gd
@@ -2,20 +2,23 @@ extends PathFollow2D
 var velocity = Vector2.ZERO
 @export var runSpeed = 300
 var last_position
+var tel_pos_start
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	tel_pos_start = $Pnj/AnimatedSprite2D/Telephone.position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	last_position = position[0]
 	set_progress(get_progress() + runSpeed*delta)
 	if position[0] < last_position:
-			$Pnj/AnimatedSprite2D.flip_h = true
-			$Pnj/Telephone.translate(-10)
-			
+		$Pnj/AnimatedSprite2D.flip_h = true
+		$Pnj/AnimatedSprite2D/Telephone.position = Vector2(-6,2.5)
+
+
 	if position[0] > last_position:
 		$Pnj/AnimatedSprite2D.flip_h = false
+		$Pnj/AnimatedSprite2D/Telephone.position = Vector2(tel_pos_start)
 	if progress_ratio == 1:
 		while progress_ratio != 0:
 				set_progress(get_progress() - runSpeed*delta)
