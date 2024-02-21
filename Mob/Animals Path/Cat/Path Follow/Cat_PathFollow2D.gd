18cbf3e39829accf39391cff78b2174301275792
jus2tomate83 <blefebvre595@gmail.com>
2024-02-21 16:55:02 +0100

    Cat Idle Option

--- Mob/Animals Path/Cat/Path Follow/Cat_PathFollow2D.gd
+++ Mob/Animals Path/Cat/Path Follow/Cat_PathFollow2D.gd
@@ -1,24 +1,30 @@
 extends PathFollow2D
 var velocity = Vector2.ZERO
 @export var runSpeed = 300
+@export var idle_cat = false
 var last_position
-var Cat_pos_start
+var Mouse_pos_start
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Cat_pos_start = $Cat/AnimatedSprite2D.position
+	Mouse_pos_start =$Cat/AnimatedSprite2D.position
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	last_position = position[0]
-	set_progress(get_progress() + runSpeed*delta)
-	if position[0] < last_position:
-		$Cat/AnimatedSprite2D.flip_v = true
+	if idle_cat == true:
+		$Cat/AnimatedSprite2D.play("Idle")
+	if idle_cat == false:
+		$Cat/AnimatedSprite2D.play("Run")
+		last_position = position[0]
+		set_progress(get_progress() + runSpeed*delta)
+		if position[0] < last_position:
+			$Cat/AnimatedSprite2D.flip_v = true
 		
-
-	if position[0] > last_position:
-		$Cat/AnimatedSprite2D.flip_v = false
-	if progress_ratio == 1:
-		while progress_ratio != 0:
-				set_progress(get_progress() - runSpeed*delta)
+		if position[0] > last_position:
+			$Cat/AnimatedSprite2D.flip_v = false
+		if progress_ratio == 1:
+			while progress_ratio != 0:
+					set_progress(get_progress() - runSpeed*delta)
 
