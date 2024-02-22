e88c69124ad3754a99cd01e474f4dc8ada68bc0f
jus2tomate83 <blefebvre595@gmail.com>
2024-02-22 18:56:24 +0100

    Add cat Box

--- Mob/Animals Path/Cat/Path Follow/Cat_PathFollow2D.gd
+++ Mob/Animals Path/Cat/Path Follow/Cat_PathFollow2D.gd
@@ -2,6 +2,7 @@ extends PathFollow2D
 var velocity = Vector2.ZERO
 @export var runSpeed = 300
 @export var idle_cat = false
+@export var Cat_box = false
 var last_position
 var Mouse_pos_start
 
@@ -10,18 +11,18 @@ var Mouse_pos_start
 func _ready():
 	Mouse_pos_start =$Cat/AnimatedSprite2D.position
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if idle_cat == true:
 		$Cat/AnimatedSprite2D.play("Idle")
-	if idle_cat == false:
+	if Cat_box == true:
+		$Cat/AnimatedSprite2D.play("CatBox")
+	if idle_cat == false and Cat_box == false:
 		$Cat/AnimatedSprite2D.play("Run")
 		last_position = position[0]
 		set_progress(get_progress() + runSpeed*delta)
 		if position[0] < last_position:
 			$Cat/AnimatedSprite2D.flip_v = true
-		
 		if position[0] > last_position:
 			$Cat/AnimatedSprite2D.flip_v = false
 		if progress_ratio == 1:
