6935c478fbf43e7baffee0c5ae9cd837c28a2cf5
jus2tomate83 <blefebvre595@gmail.com>
2024-03-04 13:22:56 +0100

    Add gun

--- Items/Weapon/Pistolet/Pistolet.gd
+++ Items/Weapon/Pistolet/Pistolet.gd
@@ -1,11 +1,18 @@
 extends Node2D
-
-
+var MousePos 
+var angle
+@onready var hand = get_node("res://Mob/Player/Player_Scène.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func rotate_gun():
+	angle = $".".position
+	angle = angle.angle()
+	$".".look_at(get_global_mouse_position())
+	$".".rotate(-angle)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	rotate_gun()
+	
--- Mob/Player/Player_Script.gd
+++ Mob/Player/Player_Script.gd
@@ -1,13 +1,22 @@
 extends CharacterBody2D
 
-const SPEED = 200
+const SPEED = 250
 const JUMP_VELOCITY = -200
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	gravity = 0
+
+func equip():
+	var pistol = preload("res://Items/Weapon/Pistolet/Pistolet.tscn").instantiate()
+	if Input.is_action_just_pressed('Equiper'):
+		$"AnimatedSprite2D".add_child(pistol)
+		print("Equipé !")
+	
 
 func _physics_process(delta):
+	equip()
 	var direction = Input.get_axis("gauche", "droite")
-	
+	print(gravity)
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	if Input.is_action_just_pressed("jump") and is_on_floor():
